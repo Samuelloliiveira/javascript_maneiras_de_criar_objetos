31cbeb268f2556887036fd687465b58f9f6634c3
samuel <samuelcontatodev@gmail.com>
2021-07-04 19:19:41 -0300

    factory

--- factory.js
+++ factory.js
@@ -1,1 +1,32 @@
 //FACTORY = Padrão de projeto, significa Fabrica
+
+function createMicrofone(color = 'black') {
+    let isOn= true
+
+    return {
+        color,
+        isOn,
+
+        toggleOnOff() {
+            if(isOn) {
+                console.log('desligar')
+            }else{
+                console.log('ligar');
+            }
+
+            isOn = !isOn
+        }
+    }
+}
+
+
+const microfoneBlack = createMicrofone()//Começa como preto
+const microfoneWhite = createMicrofone('white')
+
+console.log(microfoneBlack.color)
+console.log(microfoneWhite.color)
+
+//Pode colocar o object literal como retorno
+//Mantém a variável dentro do escopo da função
+
+//contra: recriação de funções internas
